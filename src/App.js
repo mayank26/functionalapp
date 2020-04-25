478c9368dbd7b0be0a1c3366c67d7d3555f37e14
mayank26 <mayank.gupta679@gmail.com>
2020-04-25 14:06:00 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {Youtube} from "./Youtube"
+
+function App() {
+  return (
+    <div className="App">
+     <Youtube/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Youtube.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+
+export const Youtube = () => {
+    return <h1>Hello my Youtube Family</h1>
+}
+
