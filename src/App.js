6bc51c693f7e8dcaaddaecc56ad5f9bd8b70dd24
mayank26 <mayank.gupta679@gmail.com>
2020-04-25 22:44:35 +0530

    Class component added

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {Youtube} from "./Youtube"
+import Developer from "./Developer"
 
 function App() {
   return (
     <div className="App">
      <Youtube/>
+     <Developer/>
     </div>
   );
 }
---
+++ src/Developer.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react'
+
+class Developer extends Component {
+    render() {
+        return <h1>Hello Developers</h1>
+    }
+}
+
+export default Developer
